09f90952bf17f73f41105c99d0726d2e4b978419
starrpatty28 <starrpatty28@gmail.com>
2019-11-28 00:52:23 -0800

    Create my header with Style, create my grid and grid columns, imput form with icon

--- react-slack-clone/src/components/Auth/Register.js
+++ react-slack-clone/src/components/Auth/Register.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import {  Grid, Form, Button, Header, Message, Icon } from 'semantic-ui-react';
+import {  Grid, Form, Button, Header, Message, Icon, Segment } from 'semantic-ui-react';
 
 // stateful component
 class Register extends React.Component {
+  handleChange = () => {}
+
   render() {
     return (
-      <div>Register</div>
+      <Grid textAlign="center" verticalAlign="middle">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece" color="orange" />
+            Register for BlackChat
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input fluid name="username" icon="user" iconPosition="left" placeHolder="Username" onChange={this.handleChange} />
+            </Segment>
+          </Form>
+        </Grid.Column>
+      </Grid>
     )
   }
 }
