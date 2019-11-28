ce20d0bc67d348e14f555d8c65181eae846dade3
starrpatty28 <starrpatty28@gmail.com>
2019-11-27 22:54:40 -0800

    Created Routes for the login and Register

--- react-slack-clone/src/index.js
+++ react-slack-clone/src/index.js
@@ -5,12 +5,15 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Register from './components/Auth/Register';
 
 //house my routes in a stateless functional component
 const Root = () => (
   <Router>
     <Switch>
       <Route path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
     </Switch>
   </Router>
 )
