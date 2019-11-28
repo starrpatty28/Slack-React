dd57a0bc965595f6fc233777f0ecbe7e0c4d44db
starrpatty28 <starrpatty28@gmail.com>
2019-11-28 01:47:22 -0800

    Created a username, email, email confirmation input tag, submit button, link to login

--- react-slack-clone/src/components/Auth/Register.js
+++ react-slack-clone/src/components/Auth/Register.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {  Grid, Form, Button, Header, Message, Icon, Segment } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 // stateful component
 class Register extends React.Component {
@@ -7,17 +8,25 @@ class Register extends React.Component {
 
   render() {
     return (
-      <Grid textAlign="center" verticalAlign="middle">
+      <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h2" icon color="orange" textAlign="center">
-            <Icon name="puzzle piece" color="orange" />
-            Register for BlackChat
+          <Header as="h2" icon color="black" textAlign="center">
+            <Icon name="wechat" color="black" />
+            Register for BlackDevChat
           </Header>
           <Form size="large">
             <Segment stacked>
-              <Form.Input fluid name="username" icon="user" iconPosition="left" placeHolder="Username" onChange={this.handleChange} />
+              <Form.Input fluid name="username" icon="user" iconPosition="left" placeHolder="Username" onChange={this.handleChange} type="text" />
+
+              <Form.Input fluid name="email" icon="mail" iconPosition="left" placeHolder="Email Address" onChange={this.handleChange} type="email" /> 
+
+              <Form.Input fluid name="password" icon="lock" iconPosition="left" placeHolder="Password" onChange={this.handleChange} type="password" /> 
+              <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" placeHolder="Password" onChange={this.handleChange} type="password" /> 
+
+              <button class="ui secondary button" color="black" fluid size="large">Submit</button>
             </Segment>
           </Form>
+          <Message>Already a user<Link to="/login"> Login</Link></Message>
         </Grid.Column>
       </Grid>
     )
